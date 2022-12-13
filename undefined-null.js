897f2ee069d785d93c9fca6d4fa6cd990487194f
salman-saleihin <saleihinkhan@gmail.com>
2022-12-13 18:11:41 +0600

    Undefined Vs Null Explaination

---
+++ undefined-null.js
@@ -0,0 +1,32 @@
+// UNDEFINED TYPES
+
+let something ;
+console.log(something);
+
+function add(num1 , num2){
+    console.log(num1+num2);
+    return 
+}
+const result1 = add(12, 8);
+console.log(result1);
+
+
+function add(num3 , num4){
+    console.log(num4);
+}
+const result2 = add(12);
+console.log(result2);
+
+const premik = {name:"smart dude" , phone: 01741367947 , /*gf: 'ALia'*/ };
+console.log(premik.gf);
+
+
+// UNDEFINED is Negative or Falsy. it can be found by many ways like-- not declaring variable, trying to access property which doesn't exist , access a element from a Array which is not in Array.. etc etc shows "Undefined" in Console!!
+
+
+// NULL TYPES -- 
+
+// NULL means not existent or Empty/"Khali" in bengali.. and this null is set by a developer in a Code.
+
+//
+
