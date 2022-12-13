5ead70edda40e411083878e4ce99e0d0ebc6b912
salman-saleihin <saleihinkhan@gmail.com>
2022-12-13 17:40:31 +0600

    JS True-False Concept Depth

---
+++ true-false.js
@@ -0,0 +1,20 @@
+// FALSE    
+
+//false
+// 0
+// '' or ""
+// undefined
+// null
+// NaN (not a number)
+
+//TRUTH
+
+// '0' , {} , [] , " "
+
+let name = 0 ;
+if(name || name == 0){
+    console.log("Condition Is True");
+}
+else{
+    console.log("Condition Is False");
+}
