6adbce00efc667256349e506102225d48a37a6ca
salman-saleihin <saleihinkhan@gmail.com>
2022-12-17 17:48:39 +0600

    Array Slice , Splice & Join Details Cleared

---
+++ array-split.js
@@ -0,0 +1,23 @@
+const numbers = [1,2,3,4,5,6,7,8] ;
+
+const part = numbers.slice(2, 5) ; /* kon index theke kon index rakhbe */
+
+// console.log(numbers);
+console.log(part);
+
+const nums = [1,2,3,4,5,6,7,8] ;
+
+const removed = nums.splice(2,3, 77 , 78 , 79) ;
+
+// console.log(removed);
+console.log(nums);
+
+const together = numbers.join(" & ");
+console.log(together);
+
+
+
+
+
+
+
